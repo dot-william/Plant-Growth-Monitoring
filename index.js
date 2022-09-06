c490983ce4f0b68f90cfa2d0c6af9854a57be494
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2022-09-06 20:28:03 +0800

    added db variables and started broker

--- index.js
+++ index.js
@@ -6,12 +6,25 @@ const port = 1883;
 //Express Server and MySQL 
 const express = require('express');
 const db = require('./models/db.js');
+const helper  = require('./helperFunction');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 var hostname = "localhost";
 
 // App set-up
 const app = express();
+
+//Valid topics and type to check
+const validTopics = ["sensor", "dlsu", "node-1"]
+const validTopicTypes = ["lightintensity", "temperature", "humidity", "soilmoisture", "ph", "images"]
+
+// DB variables
+const db_name = `pgmsdb`;
+const experiment = `cherrytomato`;
+const address = `dlsu`;
+const node = `sensornode`;
+const location = address + '_' + experiment + '_' + node;
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
 
@@ -22,8 +35,28 @@ server.listen(port, function(){
     console.log('server started and listening on port ', port);
 });
 
-app.listen(port_app, hostname, ()=> {
+app.listen(port_app, hostname, () => {
     console.log(`Server running at:`);
     console.log('http://' + hostname + ':' + port_app);
 });
-  
+
+// Broker
+aedes.on('publish', async function(packet, client) {
+    isValidTopic = helper.checkTopic(packet.topic, validTopics, validTopicTypes);
+    
+    if(client && isValidTopic) {
+        if(!packet.topic.includes("images")) {
+            var raw_data = JSON.parse(packet.payload.toString());
+            
+        } else
+            helper.logMessage("Image saved locally.");
+        
+
+
+    } else if (client && !isValidTopic) {
+        let errorMsg = "Packet format incorrect. Length not at least 4.";
+        let errorLog = helper.errorLog(errorMsg);
+        helper.logMessage(errorMsg);
+        db.insertTable(msg, "error_msg");
+    }
+});
