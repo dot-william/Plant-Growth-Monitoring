58e3ef7e73650b0d4c71675f9fda5aab4d11816e
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2022-09-18 13:23:10 +0800

    test different file extension

--- python_files/groupimages.py
+++ python_files/groupimages.py
@@ -1,6 +1,6 @@
 import os
 
-EXTENSIONS = ['.png']
+EXTENSIONS = ['.jpg']
 PATH_NAME = "/home/student/Test_Images"
 
 def get_files_list(root_dir, E):
@@ -8,6 +8,7 @@ def get_files_list(root_dir, E):
 
     for root, directories, filenames in os.walk(root_dir):
         for filename in filenames:
+            #If png is in filename
             if any(ext in filename for ext in E):
                 print(E)
                 print(root)
