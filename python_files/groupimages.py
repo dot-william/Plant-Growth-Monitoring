ff14c9c4fc603842ae7feb1971cf1a151bee624c
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2022-09-18 14:15:13 +0800

    image timestamp

--- python_files/groupimages.py
+++ python_files/groupimages.py
@@ -3,19 +3,26 @@ import os
 EXTENSIONS = ['.png', '.jpg']
 PATH_NAME = "/home/student/Test_Images"
 
+# Procedure for parsing filenames
+def image_timestamp(filename):
+    # Example filename: "IMG_IMG_20211218_100625.jpg"
+    date = filename.split("_")[1]
+    time = filename.split("_")[2].split(".")[0] # split time and file extension
+    return date, time
+
+# Finding files with specified extensions
 def get_files_list(root_dir, E):
-    file_list = []
+    file_list, file_list_parsed = [], []
 
     for root, directories, filenames in os.walk(root_dir):
         for filename in filenames:
             #If png is in filename
             if any(ext in filename for ext in E):
-                print(E)
-                print(root)
-                print(filename)
                 file_list.append(os.path.join(root, filename))
-    return file_list
+                file_list_parsed.append(image_timestamp(filename))
 
-my_images = get_files_list(PATH_NAME, EXTENSIONS)
+    return file_list, file_list_parsed
 
-print(my_images, len(my_images))
+my_images, my_images_parsed = get_files_list(PATH_NAME, EXTENSIONS)
+
+print(my_images_parsed, len(my_images_parsed))
