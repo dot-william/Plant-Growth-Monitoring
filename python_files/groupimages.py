5b0544112af87cff37d4bbc59fe69c5ee17d99b6
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2022-09-18 15:16:53 +0800

    included pandas to sort filenames

--- python_files/groupimages.py
+++ python_files/groupimages.py
@@ -1,32 +1,48 @@
 import os
 from operator import itemgetter
+import pandas as pd
+
 EXTENSIONS = ['.png', '.jpg']
 PATH_NAME = "/home/student/Test_Images"
 # PATH_NAME = r"C:\Users\willi\Desktop\_Thesis\Test_images"
-# Procedure for parsing filenames
-def image_timestamp(filename):
-    # Example filename: "IMG_IMG_20211218_100625.jpg"
 
+# Gets date value from the string
+def get_date(date):
+    return date[:4], date[4:6], date[6:8]
+
+def get_time(time):
+    time_str = time[:2] + ':' + time[2:4] + ':' + time[4:6]
+    return time_str 
+
+# Procedure for parsing filenames
+def image_datetime(filename):
+    # Example filename: "IMG_IMG_20211218_100625.jpg"
     date = filename.split("_")[1]
     time = filename.split("_")[2].split(".")[0] # split time and file extension
     return date, time
 
 # Finding files with specified extensions
 def get_files_list(root_dir, E):
-    file_list, file_list_parsed = [], []
+    file_list, file_list_parsed, timestamps = [], [], []
 
     for root, directories, filenames in os.walk(root_dir):
         for filename in filenames:
             print(filename)
             #If png is in filename
             if any(ext in filename for ext in E):
-                
                 file_list.append(os.path.join(root, filename))
-                file_list_parsed.append(image_timestamp(filename))
+                img_date, img_time = image_datetime(filename)
+                file_list_parsed.append((img_date, img_time))
+                d = {}
+                d['Year'], d['Month'], d['Day'] = get_date(img_date)
+                d['Time'] = get_time(img_time)
+                timestamps.append(dict(d))
 
-    return file_list, file_list_parsed
+    return file_list, file_list_parsed, timestamps
 
-my_images, my_images_parsed = get_files_list(PATH_NAME, EXTENSIONS)
+# Main function
+my_images, my_images_parsed, timestamps = get_files_list(PATH_NAME, EXTENSIONS)
 
-my_images_parsed = sorted(my_images_parsed, key=itemgetter(0,1)) # sort to be sure that the images are in chronological order
+my_images_parsed = sorted(my_images_parsed, key=itemgetter(0,1)) # sort by date then time
 print(my_images_parsed, len(my_images_parsed))
+print(pd.DataFrame(timestamps))
