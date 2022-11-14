f738927ad21bc2b8a362ccee3a82babf92d7a018
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2022-11-14 21:07:12 +0800

    initial mysql

--- db_api.py
+++ db_api.py
@@ -1,8 +1,12 @@
 import mysql.connector
 
-# db = mysql.connector.connect(
-#     host = 'localhost',
-#     user='blast', 
-#     password='shift12345', 
-#     database='pgmsdb')
+db = mysql.connector.connect(
+    host = 'localhost',
+    user='blast', 
+    password='shift12345', 
+    database='pgmsdb')
 
+mycusor = db.cursor()
+
+
+print("Hello world!")
