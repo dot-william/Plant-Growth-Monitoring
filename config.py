582ea80ed85f42d5840b21ed4f44c690e65823a3
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2023-01-03 16:44:05 +0800

    edited broker

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-mqttIP = "ccscloud2.dlsu.edu.ph"
-mqttPort = 20010
+# mqttIP = "ccscloud2.dlsu.edu.ph"
+# mqttPort = 20010
 
 
-# mqttIP = "mqtt.eclipseprojects.io"
-# mqttPort = 1883
+mqttIP = "mqtt.eclipseprojects.io"
+mqttPort = 1883
