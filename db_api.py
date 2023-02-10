602048cc4f1fff195d3922509ec378e619aec8e0
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2023-02-10 21:53:39 +0800

    test

--- db_api.py
+++ db_api.py
@@ -22,7 +22,7 @@ import pymysql.cursors
 # 1. Function that would get values of a type of data (e.g. temp, soil moisture) and indicated a sensor_idx and displays to terminal the number of rows gotten
 def getValues(table_name, sensor_idx, type):
 
-    query = "SELECT * from %s WHERE (sensor_idx = %s AND type = %s  ORDER BY id ASC"
+    
     connection = pymysql.connect(host='localhost', 
                                  user='blast', 
                                  password='shift12345', 
@@ -31,9 +31,10 @@ def getValues(table_name, sensor_idx, type):
     sqlArray = []
     with connection:
         with connection.cursor() as cursor:
+            query = "SELECT * from %s WHERE (sensor_idx = %s AND type = %s  ORDER BY id ASC"
             sqlArray = cursor.execute(query, ("test_data_table", "8", "temperature"))
-  
-    return sqlArray
+            result = cursor.fetchone()
+            print(result)
 # 2. Function gets the latest sensor data, and displays it
 # 3. Converter from data to CSV or dataframe
 # 4. Function that stores to the DB, adding data to the DB
@@ -43,5 +44,4 @@ def getValues(table_name, sensor_idx, type):
 #     connection.commit()
 
 
-values = getValues("test_data_table", "8", "temperature")
-print(values)
+getValues("test_data_table", "8", "temperature")
