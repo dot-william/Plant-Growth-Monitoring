62d738c63c1551563bc2e3ed36a486166007cbf8
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2023-03-14 14:14:49 +0800

    query datta

--- query_data.py
+++ query_data.py
@@ -1,18 +1,37 @@
-from dbb_api import *
+from db_api import *
+
+
+# def displayAllLatest(engine, table_name):
+#     sensor_types = ["temperature", "humidity", "light_intensity", "soil_moisture", "solution_pH", "solution_EC", "solution_TDS"]
+    
+#     # Number of indices per type
+#     type_idx_dict = {"temperature": 2, "humidity":, "light_intensity":9, "soil_moisture":, "solution_pH":, "solution_EC":, "solution_TDS": }
+#     for sensor in sensor_types:
 
 # create engine and use that for conencting to db
 table_name = "dlsu_cherrytomato_0"
 connection = create_engine()
 print(type(connection))
-df1 = get_values(connection, "dlsu_cherrytomato_0", "0", "temperature")
-df2 = get_values(connection, "dlsu_cherrytomato_0", "1", "temperature")
-df3 = get_all_values(connection,  "dlsu_cherrytomato_0")
-print(df1[df1["value"] == 70])
-print(df2[df2["value"] == 70])
-print(df3.max())
-print(df3[df3["value"] == 2128.33])
-df2 = get_all_values(connection, "dlsu_cherrytomato_0")
-display_latest_data(connection, table_name, "0", "temperature")
+df_hum_1 = display_latest_data(connection, "dlsu_cherrytomato_0", "1", "humidity")
+df_temp_1 = display_latest_data(connection, "dlsu_cherrytomato_0", "1", "temperature")
+df_light0 =display_latest_data(connection, "dlsu_cherrytomato_0", "0", "light_intensity")
+df_light1 =display_latest_data(connection, "dlsu_cherrytomato_0", "1", "light_intensity")
+df_light2 = display_latest_data(connection, "dlsu_cherrytomato_0", "2", "light_intensity")
+df_light3 =display_latest_data(connection, "dlsu_cherrytomato_0", "3", "light_intensity")
+df_light4 = display_latest_data(connection, "dlsu_cherrytomato_0", "4", "light_intensity")
+df_light5 =display_latest_data(connection, "dlsu_cherrytomato_0", "5", "light_intensity")
+df_light6 =display_latest_data(connection, "dlsu_cherrytomato_0", "6", "light_intensity")
+df_light7 = display_latest_data(connection, "dlsu_cherrytomato_0", "7", "light_intensity")
+df_light8 = display_latest_data(connection, "dlsu_cherrytomato_0", "8", "light_intensity")
+df_light9 =display_latest_data(connection, "dlsu_cherrytomato_0", "9", "light_intensity")
+
+# df3 = get_all_values(connection,  "dlsu_cherrytomato_0")
+# print(df1[df1["value"] == 70])
+# print(df2[df2["value"] == 70])
+# print(df3.max())
+# print(df3[df3["value"] == 2128.33])
+# df2 = get_all_values(connection, "dlsu_cherrytomato_0")
+# display_latest_data(connection, table_name, "0", "temperature")
 
 # if len(df2) != 0:
 #     print(df2.head())
