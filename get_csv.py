acc1484808bc0b234d98745cdc6be1a14415e67d
William Dimaculangan <william_dimaculangan@dlsu.edu.ph>
2023-07-02 23:29:13 +0800

    created csv getter

---
+++ get_csv.py
@@ -0,0 +1,12 @@
+from db_api import *
+current_date = dt.date.today()
+date_now = current_date.strftime('%Y-%m-%d')
+
+engine = create_engine()
+dli_df = get_all_values(engine, "dli_table_0")
+median_df = get_all_values(engine, "median_count_0")
+pred_df = get_all_values(engine, "pred_table_0")
+
+dli_df.to_csv(date_now+"dli_vals.csv")
+median_df.to_csv(date_now+"median_vals.csv")
+pred_df.to_csv(date_now+"pred_vals.csv")
